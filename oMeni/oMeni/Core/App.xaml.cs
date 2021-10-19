39c18c3030256de96e2e84dd98d5ed44714562cd
The Degman <dorianmarusic@gmail.com>
2021-10-19 03:42:24 +0200

    Add project files.

---
+++ oMeni/oMeni/Core/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using oMeni.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace oMeni
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Core.AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
